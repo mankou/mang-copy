b8ab629a588935749457875ceb6fd8cc6230c6d5
mang <ningma003@gmail.com>
2018-01-08 17:46:14 +0800

    * 如果源端目录下没有文件则不拷备

--- src/main/java/mang/tools/copy/copyfile/SimpleCopy.java
+++ src/main/java/mang/tools/copy/copyfile/SimpleCopy.java
@@ -22,8 +22,22 @@ public class SimpleCopy extends AdapterCopy {
 
 	public void realCopyDirectoryFilesToDestDirectory(String source, String dest) {
 		String processDest = this.processDest(dest);
-		FileUtil.forceMkdir(processDest);
-		FileUtil.copyFoldfilesToAnotherFold(source, processDest);
+		//如果源目录存在 并且源目录下有文件才拷备 否则不拷备
+		if(isHaveFiles(source)){
+			FileUtil.forceMkdir(processDest);
+			FileUtil.copyFoldfilesToAnotherFold(source, processDest);
+		}
+	}
+	
+	private boolean isHaveFiles(String path){
+		File sourceFile=new File(path);
+		if(sourceFile.exists()){
+			String[] files=sourceFile.list();
+			if(files!=null && files.length>0){
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public String processDest(String dest) {
